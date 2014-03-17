ad1cd1be34556ac185e0d5ec199f55fd9f2cf1b5
DevoNoel <devon.noeldetilly@gmail.com>
2014-03-17 13:12:39 -0400

    Improved carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,17 @@
 $(function() {
   $('#carousel').carouFredSel({
+    width: 990,
+		height: 300,
   	auto: true,
   	prev: '#prev',
   	next: '#next',
   	pagination: "#pager",
   	mousewheel: true,
+    items: {
+      visible: {
+  			max: 2
+			}
+    },
   	swipe: {
   		onMouse: true,
   		onTouch: true
