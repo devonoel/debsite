9f283ed129f0f9ca3ef42e36796042a0a74f2da3
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2018-09-05 15:39:04 -0400

    Style quotes and expand/collapse priority items

--- new/js/main.js
+++ new/js/main.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', function(){
+  scrollHandler();
+  priorityExpander();
+});
+
+function scrollHandler() {
   var scrollLinks = [
     { link: '#about-link', scrollTo: '#about' },
     { link: '#priorities-link', scrollTo: '#priorities' },
+    { link: '#faq-link', scrollTo: '#faq' },
     { link: '#contact-link', scrollTo: '#contact' },
   ]
 
@@ -14,4 +20,24 @@ document.addEventListener('DOMContentLoaded', function(){
       });
     });
   });
-});
+}
+
+function priorityExpander() {
+  var headers = document.querySelectorAll('.priority-header');
+
+  headers.forEach(function(header) {
+    header.addEventListener('click', function(e) {
+      var contentDiv = header.parentElement.querySelector('.priority-item');
+      var arrow = header.querySelector('.fas');
+
+      arrow.classList.toggle("fa-angle-down");
+      arrow.classList.toggle("fa-angle-up");
+
+      if (contentDiv.style.display === "block") {
+        contentDiv.style.display = "none";
+      } else {
+        contentDiv.style.display = "block";
+      }
+    })
+  });
+}
